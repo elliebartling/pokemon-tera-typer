1bba319759600a87c070d29f4eaae80a060b8eda
Ellen Marie Bartling <ellenbartling@gmail.com>
2023-02-24 19:29:12 -0800

    Fix mobile vertical spacing

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,8 @@ pokedex.setSelectedPokemon(pokedex.query)
     <div class="wrapper mt-0 sm:mt-32 sm:px-6">
       <PickerPalette v-if="pokedex.showPalette" />
 
-      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
-          <div className="lg:order-first lg:row-span-2 px-2 my-8 md:px-8">
+      <div className="grid grid-cols-1 gap-y-4 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+          <div className="lg:order-first lg:row-span-2 px-2 mt-8 md:px-8">
             <div class="wrapper px-4">
               <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
                 Not sure how to prep for your next Tera Raid? Here's a hint!
